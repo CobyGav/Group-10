8119a83af7c031184219f4159a1d779fb7218fcf
Raymart Barrameda <matbarrameda@gmail.com>
2023-01-23 09:22:22 +0800

    01232023a

--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,15 @@
-const navLinks = document.querySelectorAll('.nav-item:not(.dropdown)')
+const navLinks = document.querySelectorAll('.nav-custom')
 const menuToggle = document.getElementById('navbarMenu')
 const bsCollapse = new bootstrap.Collapse(menuToggle, {
     toggle: false
 })
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => {
-        if (bsCollapse._isShown()) {
-            bsCollapse.hide()
+navLinks.forEach((e) => {
+    e.addEventListener(
+        'click',
+        () => {
+            if (bsCollapse._isShown()) {
+                bsCollapse.hide()
+            }
         }
-    })
+    )
 })
-
-
-// const navLinks = document.querySelectorAll('.nav-item:not(.dropdown)')
-// const menuToggle = document.getElementById('navbarMenu')
-// const bsCollapse = new bootstrap.Collapse(menuToggle, {
-//     toggle: false
-// })
-// navLinks.forEach(function (l) {
-//     l.addEventListener('click', function () {
-//         if (menuToggle.classList.contains('show')) {
-//             bsCollapse.toggle()
-//         }
-//     })
-// })
