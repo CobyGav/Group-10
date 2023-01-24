140e1daa50bb9ea083bb2ab3bce65e355701b6db
Raymart Barrameda <matbarrameda@gmail.com>
2023-01-24 17:24:13 +0800

    01242023a

--- js/modal.js
+++ js/modal.js
@@ -2,25 +2,137 @@ const reservation = document.querySelector('#reservation');
 const fullName = document.querySelector('#fullName');
 const phoneNumber = document.querySelector('#phone');
 const email = document.querySelector('#email');
-const numberOfGuests = document.querySelector('#number-guests');
-const date = document.querySelector('#date');
-const time = document.querySelector('#time');
+const numberOfGuests = document.querySelector('#numberOfGuests');
+const dateAndTime = document.querySelector('#dateAndTime');
 
-const reserveInputs = [];
+const nameError = document.querySelector('#nameError');
+const phoneError = document.querySelector('#phoneError');
+const emailError = document.querySelector('#emailError');
+const guestError = document.querySelector('#guestError');
+const dateError = document.querySelector('#dateError');
+const timeError = document.querySelector('#timeError');
+
+const validateFullName = () => {
+    const nameValue = fullName.value.trim();
+    const nameValidator = /([a-zA-Z\-]+){3,}\s+([a-zA-Z\-]+){3,}/;
+
+    nameError.innerText = '';
+    if(!nameValue) {
+        nameError.style.display = 'block';
+        nameError.innerText = 'Full Name is required';
+        return false;
+    }
+    else if(!nameValidator.test(nameValue)) {
+        nameError.style.display = 'block';
+        nameError.innerText = 'Name and Surname are required';
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+const validatePhoneNumber = () => {
+    const phoneValue = phoneNumber.value.trim();
+
+    phoneError.innerText = '';
+    if(!phoneValue) {
+        phoneError.style.display = 'block';
+        phoneError.innerText = 'Phone Number is required';
+        return false;
+    }
+    else if(phoneValue.length !== 11) {
+        phoneError.style.display = 'block';
+        phoneError.innerText = 'Please enter a valid phone number';
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+const validateEmail = () => {
+    const emailValue = email.value.trim();
+    const emailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    emailError.innerText = '';
+    if(!emailValue) {
+        emailError.style.display = 'block';
+        emailError.innerText = 'Email is required';
+        return false;
+    }
+    else if(!emailValidator.test(emailValue)) {
+        emailError.style.display = 'block';
+        emailError.innerText = 'Please enter a valid email';
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+const validateNumberOfGuests = () => {
+    const guestValue = numberOfGuests.value;
+
+    guestError.innerText = '';
+    if(guestValue === '0') {
+        guestError.style.display = 'block';
+        guestError.innerText = 'Number of guests is required';
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+const validateDateAndTime = () => {
+    const dateTimeValue = dateAndTime.value;
+
+    dateError.innerText = '';
+    if(!dateTimeValue) {
+        dateError.style.display = 'block';
+        dateError.innerText = 'Date and Time are required';
+        return false;
+    }
+    else {
+        return true;
+    }
+};
 
 reservation.addEventListener(
     'submit',
     (e) => {
         e.preventDefault();
 
-        const reserve = {
+        validateFullName();
+        validatePhoneNumber();
+        validateEmail();
+        validateNumberOfGuests();
+        validateDateAndTime();
+        
+        const serviceId = 'service_bmn85vn';
+        const templateId = 'template_xevk21w';
+        const publicKey = 'Qm3ATmfXKXgB8G6_m';
+
+        const sendMail = {
             fullName: fullName.value,
             phoneNumber: phoneNumber.value,
             email: email.value,
             numberOfGuests: numberOfGuests.value,
-            date: date.value,
-            time: time.value
+            dateAndTime: dateAndTime.value
+        };
+        
+        if(validateFullName() === true && validatePhoneNumber() === true && validateEmail() === true && validateNumberOfGuests() === true && validateDateAndTime() === true && confirm('Are you sure you want to submit?') === true) {
+            // emailjs.send(serviceId, templateId, sendMail, publicKey);
+            console.log(sendMail);
+            reservation.reset();
+            $('#reserve').modal('hide');
+            setTimeout(
+                () => {
+                    alert('Your restaurant reservation has been received and we look forward to welcoming you soon.');
+                },
+                2000
+            );
         }
-        console.log(reserve);
     }
 );
